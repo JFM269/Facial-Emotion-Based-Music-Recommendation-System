dc6197c57e88284d4e1b0b7dcacf612281488350
JFM269 <87769268+JFM269@users.noreply.github.com>
2023-12-22 01:18:48 +0530

    Add files via upload

---
+++ Emotion Detection CNN Model.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('pip install kaggle')
+
+
+# In[ ]:
+
+
+get_ipython().system('mkdir ~/.kaggle')
+
+
+# In[ ]:
+
+
+cp kaggle.json ~/.kaggle/
+
+
+# In[ ]:
+
+
+get_ipython().system('kaggle datasets download -d ananthu017/emotion-detection-fer')
+
+
+# In[ ]:
+
+
+get_ipython().system('unzip /content/emotion-detection-fer.zip')
+
+
+# In[ ]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import keras
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten,Dense,Dropout,BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from tensorflow.keras.applications import VGG16, InceptionResNetV2
+from keras import regularizers
+from tensorflow.keras.optimizers import Adam,RMSprop,SGD,Adamax
+
+
+# In[ ]:
+
+
+train_dir = "/content/train" 
+test_dir = "/content/test"
+
+
+# In[ ]:
+
+
+img_size = 48
+
+
+# In[ ]:
+
+
+# Data Augmentation
+
+train_datagen = ImageDataGenerator(width_shift_range = 0.1,
+                                         height_shift_range = 0.1,
+                                         horizontal_flip = True,
+                                         rescale = 1./255,
+                                         validation_split = 0.2
+                                        )
+validation_datagen = ImageDataGenerator(rescale = 1./255,
+                                         validation_split = 0.2)
+
+
+# In[ ]:
+
+
+# Applying data augmentation to the images as we read them from their respective directories
+
+train_generator = train_datagen.flow_from_directory(directory = train_dir,
+                                                    target_size = (img_size,img_size),
+                                                    batch_size = 64,
+                                                    color_mode = "grayscale",
+                                                    class_mode = "categorical",
+                                                    subset = "training"
+                                                   )
+validation_generator = validation_datagen.flow_from_directory( directory = test_dir,
+                                                              target_size = (img_size,img_size),
+                                                              batch_size = 64,
+                                                              color_mode = "grayscale",
+                                                              class_mode = "categorical",
+                                                              subset = "validation"
+                                                             )
+
+
+# In[ ]:
+
+
+model= tf.keras.models.Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=(48, 48,1)))
+model.add(Conv2D(64,(3,3), padding='same', activation='relu' ))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128,(5,5), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+    
+model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01))) 
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(512,(3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten()) 
+model.add(Dense(256,activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+    
+model.add(Dense(512,activation = 'relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+
+model.add(Dense(7, activation='softmax'))
+
+model.compile(
+    optimizer = Adam(lr=0.0001), 
+    loss='categorical_crossentropy', 
+    metrics=['accuracy']
+  )
+
+
+# In[ ]:
+
+
+epochs = 60
+batch_size = 64
+
+
+# In[ ]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+history = model.fit(x = train_generator,epochs = epochs,validation_data = validation_generator)
+
+
+# In[ ]:
+
+
+fig , ax = plt.subplots(1,2)
+train_acc = history.history['accuracy']
+train_loss = history.history['loss']
+fig.set_size_inches(12,4)
+
+ax[0].plot(history.history['accuracy'])
+ax[0].plot(history.history['val_accuracy'])
+ax[0].set_title('Training Accuracy vs Validation Accuracy')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_xlabel('Epoch')
+ax[0].legend(['Train', 'Validation'], loc='upper left')
+
+ax[1].plot(history.history['loss'])
+ax[1].plot(history.history['val_loss'])
+ax[1].set_title('Training Loss vs Validation Loss')
+ax[1].set_ylabel('Loss')
+ax[1].set_xlabel('Epoch')
+ax[1].legend(['Train', 'Validation'], loc='upper left')
+
+plt.show()
+
+
+# In[ ]:
+
+
+model.save('model_optimal.h5')
+
+
+# In[ ]:
+
+
+img = tf.keras.utils.load_img("/content/test/happy/im1021.png",target_size = (48,48),color_mode = "grayscale")
+img = np.array(img)
+plt.imshow(img)
+print(img.shape) #prints (48,48) that is the shape of our image
+
+
+# In[ ]:
+
+
+label_dict = {0:'Angry',1:'Disgust',2:'Fear',3:'Happy',4:'Neutral',5:'Sad',6:'Surprise'}
+
+
+# In[ ]:
+
+
+img = np.expand_dims(img,axis = 0) #makes image shape (1,48,48)
+img = img.reshape(1,48,48,1)
+result = model.predict(img)
+result = list(result[0])
+print(result)
+
+
+# In[ ]:
+
+
+img_index = result.index(max(result))
+print(label_dict[img_index])
+plt.show()
+
+
+# In[6]:
+
+
+train_loss, train_acc = model.evaluate(train_generator)
+test_loss, test_acc   = model.evaluate(validation_generator)
+print("final train accuracy = {:.2f} , validation accuracy = {:.2f}".format(train_acc*100, test_acc*100))
+
+
+# In[ ]:
+
+
+model.save_weights('model_weights.h5')
+
